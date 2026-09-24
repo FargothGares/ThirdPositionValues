a5385aeb2f4bf3222d1a3938a30594ac9bd64f84
FargothGares <137458462+FargothGares@users.noreply.github.com>
2026-09-24 17:11:03 +1000

    Major update including match viewer & short/random quiz options

--- ideologies.js
+++ ideologies.js
@@ -1,146 +1,268 @@
 ideologies = [
 	{
         "name": "Fascism",
+        "desc": "",
         "stats": {
-            "b1": 60,
-            "b2": 60,
-            "b3": 25,
-            "b4": 30,
-            "b5": 70,
-            "b6": 80,
-            "b7": 100,
-            "b8": 50,
+            "b1": 60, // Practical Axis, in favour of Revolution
+            "b2": 60, // Class Axis, in favour of Domination
+            "b3": 25, // Diplomacy Axis, in favour of Autarky
+            "b4": 30, // Identity Axis, in favour of Citizenship
+            "b5": 70, // Culture Axis, in favour of Transgressive
+            "b6": 80, // Technology Axis, in favour of Development
+            "b7": 100, // Political Axis, in favour of Totality
+            "b8": 50, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "British Fascism",
+        "desc": "",
         "stats": {
-            "b1": 40,
-            "b2": 50,
-            "b3": 25,
-            "b4": 30,
-            "b5": 50,
-            "b6": 80,
-            "b7": 90,
-            "b8": 50,
+            "b1": 40, // Practical Axis, in favour of Revolution
+            "b2": 50, // Class Axis, in favour of Domination
+            "b3": 25, // Diplomacy Axis, in favour of Autarky
+            "b4": 30, // Identity Axis, in favour of Citizenship
+            "b5": 50, // Culture Axis, in favour of Transgressive
+            "b6": 80, // Technology Axis, in favour of Development
+            "b7": 90, // Political Axis, in favour of Totality
+            "b8": 50, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "National Socialism",
+        "desc": "",
         "stats": {
-            "b1": 50,
-            "b2": 50,
-            "b3": 25,
-            "b4": 0,
-            "b5": 50,
-            "b6": 60,
-            "b7": 80,
-            "b8": 50,
+            "b1": 50, // Practical Axis, in favour of Revolution
+            "b2": 50, // Class Axis, in favour of Domination
+            "b3": 25, // Diplomacy Axis, in favour of Autarky
+            "b4": 0, // Identity Axis, in favour of Citizenship
+            "b5": 50, // Culture Axis, in favour of Transgressive
+            "b6": 60, // Technology Axis, in favour of Development
+            "b7": 80, // Political Axis, in favour of Totality
+            "b8": 50, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Prussian Socialism",
+        "desc": "",
         "stats": {
-            "b1": 50,
-            "b2": 60,
-            "b3": 35,
-            "b4": 45,
-            "b5": 40,
-            "b6": 50,
-            "b7": 90,
-            "b8": 50,
+            "b1": 50, // Practical Axis, in favour of Revolution
+            "b2": 60, // Class Axis, in favour of Domination
+            "b3": 35, // Diplomacy Axis, in favour of Autarky
+            "b4": 45, // Identity Axis, in favour of Citizenship
+            "b5": 40, // Culture Axis, in favour of Transgressive
+            "b6": 50, // Technology Axis, in favour of Development
+            "b7": 90, // Political Axis, in favour of Totality
+            "b8": 50, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Integralism",
+        "desc": "",
         "stats": {
-            "b1": 65,
-            "b2": 30,
-            "b3": 50,
-            "b4": 60,
-            "b5": 25,
-            "b6": 60,
-            "b7": 70,
-            "b8": 20,
+            "b1": 65, // Practical Axis, in favour of Revolution
+            "b2": 30, // Class Axis, in favour of Domination
+            "b3": 50, // Diplomacy Axis, in favour of Autarky
+            "b4": 60, // Identity Axis, in favour of Citizenship
+            "b5": 25, // Culture Axis, in favour of Transgressive
+            "b6": 60, // Technology Axis, in favour of Development
+            "b7": 70, // Political Axis, in favour of Totality
+            "b8": 20, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Distributism",
+        "desc": "",
         "stats": {
-            "b1": 25,
-            "b2": 10,
-            "b3": 60,
-            "b4": 50,
-            "b5": 25,
-            "b6": 40,
-            "b7": 40,
-            "b8": 45,
+            "b1": 25, // Practical Axis, in favour of Revolution
+            "b2": 10, // Class Axis, in favour of Domination
+            "b3": 60, // Diplomacy Axis, in favour of Autarky
+            "b4": 50, // Identity Axis, in favour of Citizenship
+            "b5": 25, // Culture Axis, in favour of Transgressive
+            "b6": 40, // Technology Axis, in favour of Development
+            "b7": 40, // Political Axis, in favour of Totality
+            "b8": 45, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Falangism",
+        "desc": "",
         "stats": {
-            "b1": 70,
-            "b2": 75,
-            "b3": 45,
-            "b4": 60,
-            "b5": 35,
-            "b6": 60,
-            "b7": 75,
-            "b8": 30,
+            "b1": 70, // Practical Axis, in favour of Revolution
+            "b2": 75, // Class Axis, in favour of Domination
+            "b3": 45, // Diplomacy Axis, in favour of Autarky
+            "b4": 60, // Identity Axis, in favour of Citizenship
+            "b5": 35, // Culture Axis, in favour of Transgressive
+            "b6": 60, // Technology Axis, in favour of Development
+            "b7": 75, // Political Axis, in favour of Totality
+            "b8": 30, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Strasserism",
+        "desc": "",
         "stats": {
-            "b1": 50,
-            "b2": 65,
-            "b3": 20,
-            "b4": 30,
-            "b5": 50,
-            "b6": 50,
-            "b7": 75,
-            "b8": 60,
+            "b1": 50, // Practical Axis, in favour of Revolution
+            "b2": 65, // Class Axis, in favour of Domination
+            "b3": 20, // Diplomacy Axis, in favour of Autarky
+            "b4": 30, // Identity Axis, in favour of Citizenship
+            "b5": 50, // Culture Axis, in favour of Transgressive
+            "b6": 50, // Technology Axis, in favour of Development
+            "b7": 75, // Political Axis, in favour of Totality
+            "b8": 60, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Legionarism",
+        "desc": "",
         "stats": {
-            "b1": 75,
-            "b2": 60,
-            "b3": 45,
-            "b4": 30,
-            "b5": 40,
-            "b6": 50,
-            "b7": 90,
-            "b8": 10,
+            "b1": 75, // Practical Axis, in favour of Revolution
+            "b2": 60, // Class Axis, in favour of Domination
+            "b3": 45, // Diplomacy Axis, in favour of Autarky
+            "b4": 30, // Identity Axis, in favour of Citizenship
+            "b5": 40, // Culture Axis, in favour of Transgressive
+            "b6": 50, // Technology Axis, in favour of Development
+            "b7": 90, // Political Axis, in favour of Totality
+            "b8": 10, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Juche",
+        "desc": "",
         "stats": {
-            "b1": 80,
-            "b2": 75,
-            "b3": 75,
-            "b4": 35,
-            "b5": 70,
-            "b6": 80,
-            "b7": 90,
-            "b8": 70,
+            "b1": 80, // Practical Axis, in favour of Revolution
+            "b2": 75, // Class Axis, in favour of Domination
+            "b3": 75, // Diplomacy Axis, in favour of Autarky
+            "b4": 35, // Identity Axis, in favour of Citizenship
+            "b5": 70, // Culture Axis, in favour of Transgressive
+            "b6": 80, // Technology Axis, in favour of Development
+            "b7": 90, // Political Axis, in favour of Totality
+            "b8": 70, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Ba'athism",
+        "desc": "",
+        "stats": {
+            "b1": 60, // Practical Axis, in favour of Revolution
+            "b2": 75, // Class Axis, in favour of Domination
+            "b3": 25, // Diplomacy Axis, in favour of Autarky
+            "b4": 40, // Identity Axis, in favour of Citizenship
+            "b5": 50, // Culture Axis, in favour of Transgressive
+            "b6": 80, // Technology Axis, in favour of Development
+            "b7": 70, // Political Axis, in favour of Totality
+            "b8": 70, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Third International Theory",
+        "desc": "",
+        "stats": {
+            "b1": 80, // Practical Axis, in favour of Revolution
+            "b2": 80, // Class Axis, in favour of Domination
+            "b3": 60, // Diplomacy Axis, in favour of Autarky
+            "b4": 25, // Identity Axis, in favour of Citizenship
+            "b5": 55, // Culture Axis, in favour of Transgressive
+            "b6": 80, // Technology Axis, in favour of Development
+            "b7": 55, // Political Axis, in favour of Totality
+            "b8": 50, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Peronism",
+        "desc": "",
+        "stats": {
+            "b1": 40, // Practical Axis, in favour of Revolution
+            "b2": 25, // Class Axis, in favour of Domination
+            "b3": 30, // Diplomacy Axis, in favour of Autarky
+            "b4": 70, // Identity Axis, in favour of Citizenship
+            "b5": 60, // Culture Axis, in favour of Transgressive
+            "b6": 80, // Technology Axis, in favour of Development
+            "b7": 50, // Political Axis, in favour of Totality
+            "b8": 50, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Yellow Socialism",
+        "desc": "",
+        "stats": {
+            "b1": 25, // Practical Axis, in favour of Revolution
+            "b2": 10, // Class Axis, in favour of Domination
+            "b3": 60, // Diplomacy Axis, in favour of Autarky
+            "b4": 30, // Identity Axis, in favour of Citizenship
+            "b5": 40, // Culture Axis, in favour of Transgressive
+            "b6": 80, // Technology Axis, in favour of Development
+            "b7": 60, // Political Axis, in favour of Totality
+            "b8": 40, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Klan Nationalism",
+        "desc": "",
+        "stats": {
+            "b1": 25, // Practical Axis, in favour of Revolution
+            "b2": 50, // Class Axis, in favour of Domination
+            "b3": 80, // Diplomacy Axis, in favour of Autarky
+            "b4": 0, // Identity Axis, in favour of Citizenship
+            "b5": 20, // Culture Axis, in favour of Transgressive
+            "b6": 60, // Technology Axis, in favour of Development
+            "b7": 70, // Political Axis, in favour of Totality
+            "b8": 10, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Superfascism",
+        "desc": "",
+        "stats": {
+            "b1": 80, // Practical Axis, in favour of Revolution
+            "b2": 90, // Class Axis, in favour of Domination
+            "b3": 30, // Diplomacy Axis, in favour of Autarky
+            "b4": 20, // Identity Axis, in favour of Citizenship
+            "b5": 0, // Culture Axis, in favour of Transgressive
+            "b6": 50, // Technology Axis, in favour of Development
+            "b7": 80, // Political Axis, in favour of Totality
+            "b8": 10, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Fourth Political Theory",
+        "desc": "",
+        "stats": {
+            "b1": 80, // Practical Axis, in favour of Revolution
+            "b2": 40, // Class Axis, in favour of Domination
+            "b3": 15, // Diplomacy Axis, in favour of Autarky
+            "b4": 60, // Identity Axis, in favour of Citizenship
+            "b5": 5, // Culture Axis, in favour of Transgressive
+            "b6": 40, // Technology Axis, in favour of Development
+            "b7": 80, // Political Axis, in favour of Totality
+            "b8": 10, // Religious Axis, in favour of Material
+        }
+    },
+    {
+        "name": "Neosocialism",
+        "desc": "",
+        "stats": {
+            "b1": 70, // Practical Axis, in favour of Revolution
+            "b2": 30, // Class Axis, in favour of Domination
+            "b3": 50, // Diplomacy Axis, in favour of Autarky
+            "b4": 40, // Identity Axis, in favour of Citizenship
+            "b5": 75, // Culture Axis, in favour of Transgressive
+            "b6": 85, // Technology Axis, in favour of Development
+            "b7": 70, // Political Axis, in favour of Totality
+            "b8": 75, // Religious Axis, in favour of Material
         }
     },
     {
         "name": "Folkish Vitalism",
+        "desc": "",
         "stats": {
-            "b1": 80,
-            "b2": 90,
-            "b3": 50,
-            "b4": 0,
-            "b5": 90,
-            "b6": 75,
-            "b7": 100,
-            "b8": 40,
+            "b1": 80, // Practical Axis, in favour of Revolution
+            "b2": 90, // Class Axis, in favour of Domination
+            "b3": 50, // Diplomacy Axis, in favour of Autarky
+            "b4": 0, // Identity Axis, in favour of Citizenship
+            "b5": 90, // Culture Axis, in favour of Transgressive
+            "b6": 75, // Technology Axis, in favour of Development
+            "b7": 100, // Political Axis, in favour of Totality
+            "b8": 40, // Religious Axis, in favour of Material
         }
     },
-];
-
+];  
---
+++ labelArrays.js
@@ -0,0 +1,10 @@
+labelArrays = [
+["Militant","Revolutionary","Radical","Ambivalent","Gradualist","Reformist","Pacifist"],
+["Supremacist","Dominant","Hierarchical","Ambivalent","Conciliatory","Collaborative","Integrative"],
+["Isolationist","Sovereigntist","Protectionist","Ambivalent","Cooperative","Multilateralist","Expansionist"],
+["Civic-Nationalist","Assimilationist","Cultural-Nationalist","Ambivalent","Particularist","Ethnicist","Racialist"],
+["Futurist","Iconoclastic","Progressive","Ambivalent","Conservative","Traditionalist","Reactionary"],
+["Fordist","Productivist","Industrialist","Ambivalent","Conservationist","Luddite","Primitivist"],
+["Totalitarian","Unitary","Centralist","Ambivalent","Decentralist","Pluralist","Polycentric"],
+["Antitheist","Laïcard","Secularist","Ambivalent","Religious","Clericist","Theocrat"]
+]
